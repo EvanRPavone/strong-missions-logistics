a98e5b4b93be50c7b604e55b0cfd873d84dafcb0
Evan Pavone <evanrpavone@gmail.com>
2021-05-19 09:01:00 -1000

    routes proper

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  resources :trips do
+    resources :comments
+  end
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
 
   devise_for :users
+  get 'active-sessions', to: "trips#active_sessions"
   root to: 'home#index'
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
