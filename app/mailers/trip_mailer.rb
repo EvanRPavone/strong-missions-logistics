2b21481474545080df27c66ba4445a03df0a2801
Evan Pavone <evanrpavone@gmail.com>
2021-06-02 12:12:27 -0400

    removed mailer - temp

--- app/mailers/trip_mailer.rb
+++ app/mailers/trip_mailer.rb
@@ -1,12 +1,3 @@
 class TripMailer < ApplicationMailer
-    helper :application
 
-    default from: "notifications@strongmissions.com"
-
-    def trip_scheduled
-        @trip = params[:trip]
-        @user = params[:user]
-
-        mail(to: @user.email, subject: "Strong Missions Team Registered")
-    end
 end
